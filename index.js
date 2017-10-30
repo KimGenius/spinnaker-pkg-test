feabb7e54ae017df241559462686791cf817f153
KimGenius <youngjae1047@naver.com>
2017-10-30 16:37:20 +0900

    path test

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ app.get('/', function (req, res) {
   res.send('Hello World!!!!! GENIE');
 });
 
+app.get('/dotabc',function(req,res) {
+  res.sendFile('./abc.html'); 
+});
+
 app.get('/abc',function(req,res) {
-  res.sendFile('abc.html'); 
+  res.sendFile('/abc.html'); 
 });
 
 app.get('/healthcheck', function (req, res) {
